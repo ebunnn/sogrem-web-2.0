69d6bc6d4e6a904c8ecf9b80ff4699fd6fdf80fb
Ebun O <remioludemi@gmail.com>
2020-06-16 23:37:54 -0400

    part 1 of nav bar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="Nav-bar">
+          <a href="https://www.google.com/search?q=home&oq=home+&aqs=chrome..69i57j0l4j69i61j69i60j69i61.5463j0j7&sourceid=chrome&ie=UTF-8">Home</a>
+          <a href="https://www.google.com/search?q=tax+returns&oq=tax+returns&aqs=chrome..69i57j0l7.6327j0j9&sourceid=chrome&ie=UTF-8">Tax Returns</a>
+          <a href="https://www.google.com/search?sxsrf=ALeKk02_JxKrwLOrHFwpDUh2GzrRFJ9LWA%3A1592358683627&ei=G3fpXur2JYGxytMPnPax4Aw&q=contact+us&oq=contact+us&gs_lcp=CgZwc3ktYWIQAzIFCAAQsQMyBQgAELEDMgUIABCxAzIFCAAQsQMyAggAMgUIABCxAzICCAAyAggAMgIIADICCAA6BwgjEOoCECc6BAgjECc6BQgAEIMBOgQIABBDOgcIABCxAxBDOgUIABCRAjoICAAQsQMQkQI6BAgAEApQo9wCWOLtAmD18gJoAnAAeACAAWGIAcIFkgECMTGYAQCgAQGqAQdnd3Mtd2l6sAEK&sclient=psy-ab&ved=0ahUKEwiq8MSt3ofqAhWBmHIEHRx7DMwQ4dUDCAw&uact=5">Contact Us</a>
+          <a href="https://www.google.com/search?sxsrf=ALeKk01aRxh1dCqbUjYfbT7mrmhbEq1D-w%3A1592358732046&ei=THfpXtWpArebwbkPlP6emAM&q=about+us&oq=about+us&gs_lcp=CgZwc3ktYWIQAzIHCAAQsQMQQzIECAAQQzIFCAAQsQMyAggAMgQIABBDMgIIADIECAAQQzIECAAQQzIECAAQQzICCAA6BAgAEEc6BggAEAcQHlD_3gNY3-QDYKmDBGgAcAJ4AIABdIgBwQOSAQMzLjKYAQCgAQGqAQdnd3Mtd2l6&sclient=psy-ab&ved=0ahUKEwjV_8_E3ofqAhW3TTABHRS_BzMQ4dUDCAw&uact=5">About Us</a>
+        </div>
+        <div className="main-image">
+        <img src="https://image1.masterfile.com/getImage/NjAwLTAxNjE0OTkxZW4uMDAwMDAwMDA=AA$6u$/600-01614991en_Masterfile.jpg" className="App-logo" alt="logo" />
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
