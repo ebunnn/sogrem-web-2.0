3a447b67eb1b68aea6ec99bff163649393b2996e
Ebun O <remioludemi@gmail.com>
2020-06-17 00:10:46 -0400

    Updates

---
+++ precache-manifest.103adc8b0fb09a32090176b6cf984359.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c6a754d39860b7e576970d92534f31",
+    "url": "/sogrem-web-2.0/index.html"
+  },
+  {
+    "revision": "0268db82aeef33e60825",
+    "url": "/sogrem-web-2.0/static/css/main.6b50df1e.chunk.css"
+  },
+  {
+    "revision": "e3ca62cb845dae72e0c0",
+    "url": "/sogrem-web-2.0/static/js/2.b18c695b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sogrem-web-2.0/static/js/2.b18c695b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0268db82aeef33e60825",
+    "url": "/sogrem-web-2.0/static/js/main.67c4cd16.chunk.js"
+  },
+  {
+    "revision": "8e4d030c2009940bedff",
+    "url": "/sogrem-web-2.0/static/js/runtime-main.ea64daa2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sogrem-web-2.0/static/media/logo.5d5d9eef.svg"
+  }
+]);
