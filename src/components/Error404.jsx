f48037e22a55d80a1ae8ca0756af28a88b4fd6cc
Ebun O <remioludemi@gmail.com>
2024-05-17 01:20:02 -0400

    error page updated

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -5,7 +5,7 @@ export default function Error404() {
     return (
         <div className="error404--container">
             <img style={{width:"20%"}} src={salogo} alt="404 Logo" />
-            <h1 style={{color:"black"}}>404 Page Not Found 🙁</h1>
+            <h1 style={{color:"black"}}>Page Not Found 🙁</h1>
             <p style={{color:"black"}}>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
             <br/>
             <br/>
